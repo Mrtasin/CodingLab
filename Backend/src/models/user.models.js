3e2b40a9aa031d7e906d31b1ee314c10a5687d0a
Mrtasin <mohammadtasinspj@gmail.com>
2025-12-24 23:07:38 +0530

    Create Folder Structure and Add User Model

---
+++ Backend/src/models/user.models.js
@@ -0,0 +1,54 @@
+import { Schema, Model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    avatar: {
+      type: String,
+      default: "https://placehold.co/600x400/orange/white",
+    },
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      min: 3,
+    },
+    gender: {
+      type: String,
+      enum: ["MALE", "FEMALE", "OTHER"],
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    verificationToken: String,
+    verificationExpiry: Date,
+
+    emailVerificationToken: String,
+    emailVerificationExpiry: Date,
+
+    refreshToken: String,
+  },
+  { timestamps: true }
+);
+
+const User = Model("User", userSchema);
+
+export default User;
