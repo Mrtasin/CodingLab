f95b3a2ecf06007c2b9c3da1f3ec05e2b7bf4216
Mrtasin <mohammadtasinspj@gmail.com>
2025-12-25 21:55:36 +0530

    Add problem model with schemas

---
+++ Backend/src/models/problem.models.js
@@ -0,0 +1,99 @@
+import { Schema, Model } from "mongoose";
+
+const testCaseSchema = new Schema(
+  {
+    input: {
+      type: String,
+      required: true,
+    },
+    output: {
+      type: String,
+      required: true,
+    },
+    isSample: {
+      type: Boolean,
+      default: false, // sample or hidden test case
+    },
+  },
+  { _id: false }
+);
+
+const boilerplateSchema = new Schema(
+  {
+    language: {
+      type: String,
+      enum: ["PYTHON", "CPP", "JAVASCRIPT"],
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+    },
+  },
+  { _id: false }
+);
+
+const problemSchema = new Schema(
+  {
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    difficulty: {
+      type: String,
+      enum: ["EASY", "MEDIUM", "HARD"],
+    },
+    inputFormat: {
+      type: String,
+      required: true,
+    },
+
+    outputFormat: {
+      type: String,
+      required: true,
+    },
+
+    constraints: {
+      type: String,
+      required: true,
+    },
+
+    tags: [{ type: String }],
+
+    testCases: [testCaseSchema],
+
+    boilerplateCode: [boilerplateSchema],
+    supportedLanguages: {
+      type: [String],
+      default: ["PYTHON"],
+    },
+    timeLimit: {
+      type: Number,
+      default: 1, // seconds
+    },
+
+    memoryLimit: {
+      type: Number,
+      default: 256, // MB
+    },
+
+    isPremium: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Problem = Model("Problem", problemSchema);
+
+export default Problem;
--- Backend/src/models/user.models.js
+++ Backend/src/models/user.models.js
@@ -24,6 +24,11 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
     fullname: {
       type: String,
       required: true,
