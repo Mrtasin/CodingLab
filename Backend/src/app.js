8371a57f407c19923b46a41dd91938e124573696
Mrtasin <mohammadtasinspj@gmail.com>
2025-12-31 22:29:54 +0530

    CORS configuration and properly mount user routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.routes.js";
 const app = express();
 
 app.use(express.json());
@@ -8,13 +9,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.BASE_URL,
+    origin: process.env.CORS_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
 
-app.use("/api/v1/users");
+app.use("/api/v1/users", userRoutes);
 
 export default app;
