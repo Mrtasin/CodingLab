9d268a3cb49e540014d7abed6e4f541d850f1a7d
Mrtasin <mohammadtasinspj@gmail.com>
2025-12-31 22:24:28 +0530

    Refactor user model and add email verification token methods

--- Backend/src/models/user.models.js
+++ Backend/src/models/user.models.js
@@ -1,5 +1,6 @@
 import { Schema, model } from "mongoose";
 import bcrypt from "bcryptjs";
+import crypto from "crypto";
 
 const userSchema = new Schema(
   {
@@ -47,22 +48,29 @@ const userSchema = new Schema(
     verificationToken: String,
     verificationExpiry: Date,
 
-    emailVerificationToken: String,
-    emailVerificationExpiry: Date,
+    resetVerificationToken: String,
+    resetVerificationExpiry: Date,
 
     refreshToken: String,
   },
   { timestamps: true },
 );
 
-userSchema.pre("save", async function (next) {
-  if (this.isModified("password"))
-    this.password = await bcrypt.hash(this.password, 10);
-  next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 userSchema.methods.comparePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.createEmailVerificationToken = function () {
+  const verificationToken = crypto.randomBytes(32).toString("hex");
+  this.verificationToken = verificationToken;
+  this.verificationExpiry = Date.now() + 10 * 60 * 1000; // 10 minutes
+
+  return verificationToken;
 };
 
 const User = model("User", userSchema);
