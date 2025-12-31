e712d0b3b8eb82247f382c8f33625ceceffff837
Mrtasin <mohammadtasinspj@gmail.com>
2025-12-31 22:31:38 +0530

    implement user registration with email verification

--- Backend/src/controllers/user.controllers.js
+++ Backend/src/controllers/user.controllers.js
@@ -1,1 +1,53 @@
-asyncHandler
+import asyncHandler from "../utils/asyncHandler.js";
+import User from "../models/user.models.js";
+import ApiError from "../utils/apiError.js";
+import ApiResponse from "../utils/apiResponse.js";
+import crypto from "crypto";
+import mailSender from "../utils/emailSender.js";
+
+const userRegister = asyncHandler(async (req, res) => {
+  const { fullname, email, password, gender } = req.body;
+  if (!fullname || !email || !password || !gender)
+    throw new ApiError(400, "All fields are required");
+
+  const isFound = await User.findOne({ email });
+  if (isFound)
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "This email already regesterd", isFound));
+
+  const uname = email.split("@")[0] + "-" + crypto.randomBytes(4).toString("hex");
+
+  const newUser = await User.create({
+    fullname,
+    email,
+    password,
+    gender,
+    username: uname,
+  });
+  if (!newUser) throw new ApiError(500, "Internel Server Error");
+
+  const token = newUser.createEmailVerificationToken();
+
+  if (!token) throw new ApiError(500, "Internel Server Error");
+
+  await newUser.save();
+
+  newUser.password = undefined;
+
+  const option = {
+    name: fullname,
+    email: email,
+    subject: "Email Verification",
+    instructions: `Click on the link below to verify your email address:`,
+    link: `${process.env.BASE_URL}/api/v1/users/verify/${token}`,
+  };
+
+  await mailSender(option);
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, "User register successfully", newUser));
+});
+
+export { userRegister };
