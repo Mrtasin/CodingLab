fbeefd903fff6c9e0b276a9a337c6ccf57d3dd7c
Mrtasin <mohammadtasinspj@gmail.com>
2026-01-11 09:20:43 +0530

    Add submission controller functions for handling submissions

---
+++ Backend/src/controllers/submission.controllers.js
@@ -0,0 +1,14 @@
+import ApiError from "../utils/apiError.js";
+import ApiResponse from "../utils/apiResponse.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import Submission from "../models/submission.models.js";
+
+const createSubmission = asyncHandler(async (req, res) => {});
+
+const getSubmissionById = asyncHandler(async (req, res) => {});
+
+const runCode = asyncHandler(async (req, res) => {});
+
+const getAllSubmissions = asyncHandler(async (req, res) => {});
+
+export { createSubmission, getSubmissionById, runCode, getAllSubmissions };
